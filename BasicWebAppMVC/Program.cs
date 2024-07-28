12e108e013fd71c8cd7815c6e768e65ef8f0945a
Maryam Almaskin <malmaskin@steerstudios.com>
2024-07-28 15:56:26 +0300

    Adding Autherization & Authentication

--- BasicWebAppMVC/Program.cs
+++ BasicWebAppMVC/Program.cs
@@ -1,10 +1,29 @@
+using System.Text;
 using BasicWebAppMVC.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddSingleton<IBooksLibrary, BooksLibrary>();
+
+// Configure JWT authentication
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+        };
+    });
 
 var app = builder.Build();
 
@@ -21,6 +40,9 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+// Registers the authentication middleware that uses the default authentication scheme set by AddAuthentication.
+app.UseAuthentication();
+// Registers the authorization middleware to enforce authorization policies on the app's routes.
 app.UseAuthorization();
 
 app.MapControllerRoute(
